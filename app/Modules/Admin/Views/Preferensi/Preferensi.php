39ed5330ef22fd7442ccec62b192479ecb4d1e0b
yusfi1997 <yusfiadil@gmail.com>
2021-09-16 14:03:50 +0700

    preferensi

--- app/Modules/Admin/Views/Preferensi/Preferensi.php
+++ app/Modules/Admin/Views/Preferensi/Preferensi.php
@@ -181,7 +181,7 @@
                 backdrop: 'static'
             });
 
-            $.get("<?= site_url('getdatafromdb') ?>", function(data) {
+            $.get("<?= site_url('preferensi/getdatafromdb') ?>", function(data) {
                 $("#Modalloading").modal('hide');
 
                 Swal.fire(
@@ -215,22 +215,22 @@
     document.addEventListener('contextmenu', function(e) {
         e.preventDefault();
     });
-    document.onkeydown = function(e) {
-        if (event.keyCode == 123) {
-            return false;
-        }
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-            return false;
-        }
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-            return false;
-        }
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-            return false;
-        }
-        if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-            return false;
-        }
-    }
+    // document.onkeydown = function(e) {
+    //     if (event.keyCode == 123) {
+    //         return false;
+    //     }
+    //     if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+    //         return false;
+    //     }
+    //     if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+    //         return false;
+    //     }
+    //     if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+    //         return false;
+    //     }
+    //     if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+    //         return false;
+    //     }
+    // }
 </script>
 <?= $this->endSection() ?>
