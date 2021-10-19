b0032979a7ab89338016c09e4db46f8c1be4e26f
SeptianIqbalPratama <septianiqbalpratama@gmail.com>
2021-10-19 15:08:13 +0700

    update form login

--- app/Modules/Admin/Views/Auth.php
+++ app/Modules/Admin/Views/Auth.php
@@ -1,59 +1,70 @@
 <?= $this->extend('admin/layouts/auth') ?>
 <?= $this->section('content') ?>
 
-<div class="container-fluid" style="background-image: url('<?= base_url('images/bg2.jpg'); ?>'); background-size: cover;">
+<div class="">
+	
+	<div class="bg-login">
+		<img src="<?= base_url('images/background-login.png'); ?>" alt="">
+	</div>
 
-  <div class="row">
-
-    <div class="col-md-5 login--col-left">
-		<div class="w-100 p-5">
-			<div class="form-group text-center">
-				<img src="<?= base_url('images/logo_pu.jpg') ?>" width="15%">
-			</div>
-
-			<div class="form-group text-center">
-				<h4>DIREKTORAT JENDERAL SUMBER DAYA AIR</h4>
-				<h5>Kementerian Pekerjaan Umum dan Perumahan Rakyat</h5>
-			</div>
-
-			<?php if (session()->has('error')) : ?>
-				<div class="alert alert-danger error">
-					<?= session('error') ?>
-				</div>
-			<?php endif ?>
-
-			<form class="kt-form" action="<?= site_url('auth'); ?>" method="POST">
-				<div class="form-group mb-3 <?php if (session('errors.idpengguna')) echo 'is-invalid'; ?>">
-					<label><b>ID Pengguna</b></label>
-					<input style="border:thin solid #c5c5c5;" class="form-control" type="text" placeholder="ID Pengguna" name="idpengguna" autocomplete="off" value="">
-					<div id="idpengguna-error" class="error invalid-feedback"><?= session('errors.idpengguna') ?></div>
-				</div>
-				<div class="form-group  <?php if (session('errors.sandi')) echo 'is-invalid'; ?>">
-					<label><b>Password</b></label>
-					<input style="border:thin solid #c5c5c5;" class="form-control form-control-last" type="password" placeholder="Kata Sandi" name="sandi" value="">
-					<div id="idpengguna-error" class="error invalid-feedback"><?= session('errors.sandi') ?></div>
-				</div>
-
-				<?= csrf_field() ?>
-
-				<div class="kt-login__actions">
-					<button id="kt_login_signin_submit" class="btn btn-primary btn-pill btn-elevate btn-block btn-login"> Masuk <i class="fas fa-arrow-circle-right text-white icon-login"></i></button>
-				</div>
-			</form>
-
+	<div class="form-login">
+		<div class="form-group text-center text-dark">
+			<img src="<?= base_url('images/logo_pu.jpg') ?>" width="10%" class="mb-2">
+			<h4 class="mb-0">DIREKTORAT JENDERAL SUMBER DAYA AIR</h4>
+			<h6 class="mb-0">Kementerian Pekerjaan Umum dan Perumahan Rakyat</h6>
 		</div>
-    </div>
 
-    <div class="col-md-7 login--col-right">
-		<div class="text-center w-100 p-5 text-content">
-			<h1 class="fs-100"><b>MONIKA</b></h1>
-			<h3 class="fs-31">(Monitoring Kegiatan SDA)</h3>
+		<div class="card shadow card-login">
+			<div class="card-body p-4">
+
+				<div class="mb-4 text-center">
+					<h1 class="mb-0 text-green">MONIKA</h1>
+					<h5 class="mb-0 text-green">MONITORING KEGIATAN SDA</h5>
+				</div>
+
+				<?php if (session()->has('error')) : ?>
+					<div class="alert alert-danger error">
+						<?= session('error') ?>
+					</div>
+				<?php endif ?>
+
+				<div class="card card-input-login">
+					<div class="card-body p-5">
+						<form class="kt-form" action="<?= site_url('auth'); ?>" method="POST">
+							<div class="form-group <?php if (session('errors.idpengguna')) echo 'is-invalid'; ?>">
+								<div class="input-group">
+									<div class="input-group-text bg-green mr-3">
+										<i class="text-white fas fa-user"></i>
+									</div>
+									<input style="border:thin solid #c5c5c5;" class="form-control" type="text" placeholder="ID Pengguna" name="idpengguna" autocomplete="off" value="">
+								</div>
+								<div id="idpengguna-error" class="error invalid-feedback"><?= session('errors.idpengguna') ?></div>
+							</div>
+							<div class="form-group <?php if (session('errors.sandi')) echo 'is-invalid'; ?>">
+								<div class="input-group">
+									<div class="input-group-text bg-green mr-3">
+										<i class="text-white fas fa-lock"></i>
+									</div>
+									<input style="border:thin solid #c5c5c5;" class="form-control form-control-last" type="password" placeholder="Kata Sandi" name="sandi" value="">
+								</div>
+								<div id="idpengguna-error" class="error invalid-feedback"><?= session('errors.sandi') ?></div>
+							</div>
+				
+							<?= csrf_field() ?>
+				
+							<div class="text-center">
+								<button id="kt_login_signin_submit" class="btn btn-green btn-login"> Masuk </button>
+							</div>
+						</form>
+					</div>
+				</div>
+		
+			</div>
 		</div>
-    </div>
-
-  </div>
+	</div>
 
 </div>
+
 <!-- <div class="kt-grid kt-grid--ver kt-grid--root">
 	<div class="kt-grid kt-grid--hor kt-grid--root  kt-login kt-login--v6 kt-login--signin" id="kt_login">
 		<div class="kt-grid__item kt-grid__item--fluid kt-grid kt-grid--desktop kt-grid--ver-desktop kt-grid--hor-tablet-and-mobile">
