9df57b73b063cc9d56373b0f8a1c83464063bd85
yusfi1997 <yusfiadil@gmail.com>
2021-10-25 13:43:14 +0700

    perbaikan nilai deviasi pada export excel

--- app/Modules/Admin/Views/Paket/Rekap/Rekap-1.php
+++ app/Modules/Admin/Views/Paket/Rekap/Rekap-1.php
@@ -1,75 +1,153 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rekap-Monika</title>
     <style>
-        .tableFixHead          { overflow-y: auto; height: 600px; }
-        .tableFixHead thead th { position: sticky; top: 0; border:1px solid #333;}
+        .tableFixHead {
+            overflow-y: auto;
+            height: 600px;
+        }
+
+        .tableFixHead thead th {
+            position: sticky;
+            top: 0;
+            border: 1px solid #333;
+        }
 
         /* Just common table stuff. Really. */
-        .table  { border-collapse: collapse; width: 100%; }
+        .table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
         /*th, td { padding: 8px 16px; }*/
-        .table th     { background:#f5f5f5; }
+        .table th {
+            background: #f5f5f5;
+        }
     </style>
     <style>
-    .table td, th{
-        padding:2px;
-        font-size:12px;
-    }
-    table a{color:#000099;}
-    table tr:hover{color:#000000; text-decoration: underline; font-weight: bold;}
+        .table td,
+        th {
+            padding: 2px;
+            font-size: 12px;
+        }
 
-    .tdKodeLabel{width:500px;}
-    .tdLabelFull{}
-    .tdKode{width:200px;}
-    .tdLabel{width:300px;}
-    .tdTV{width:90px;}
-    .tdLokasi{width:120px;}
-    .tdJP{width:40px;}
-    .tdMP{width:40px;}
-    .tdNilai{width:100px;}
-    .tdPersen{width:50px;}
-    .tdPersen{width:50px;}
+        table a {
+            color: #000099;
+        }
 
-    .tdsatker{background-color:#eee6ff;}
-    .tdgiat{background-color:#ddccff;}
-    .tdoutput{background-color:#ccb3ff;}
-    .tdsoutput{background-color:#bb99ff;}
-    .tdkomponen{background-color:#aa80ff;}
+        table tr:hover {
+            color: #000000;
+            text-decoration: underline;
+            font-weight: bold;
+        }
 
-    .stw0{background-color:#ff4da6; color:#FFF;}
-    .stw1{}
+        .tdKodeLabel {
+            width: 500px;
+        }
 
+        .tdLabelFull {}
+
+        .tdKode {
+            width: 200px;
+        }
+
+        .tdLabel {
+            width: 300px;
+        }
+
+        .tdTV {
+            width: 90px;
+        }
+
+        .tdLokasi {
+            width: 120px;
+        }
+
+        .tdJP {
+            width: 40px;
+        }
+
+        .tdMP {
+            width: 40px;
+        }
+
+        .tdNilai {
+            width: 100px;
+        }
+
+        .tdPersen {
+            width: 50px;
+        }
+
+        .tdPersen {
+            width: 50px;
+        }
+
+        .tdsatker {
+            background-color: #eee6ff;
+        }
+
+        .tdgiat {
+            background-color: #ddccff;
+        }
+
+        .tdoutput {
+            background-color: #ccb3ff;
+        }
+
+        .tdsoutput {
+            background-color: #bb99ff;
+        }
+
+        .tdkomponen {
+            background-color: #aa80ff;
+        }
+
+        .stw0 {
+            background-color: #ff4da6;
+            color: #FFF;
+        }
+
+        .stw1 {}
     </style>
 </head>
+
 <body>
-    <?php $colspan=11; ?>
+    <?php $colspan = 11; ?>
 
     <table class="table table-bordered">
         <tr>
-            <th align="center" colspan="<?=$colspan;?>" style="border:0px;">
-                <h3>Progres Keuangan dan Fisik - <?=$title;?></h3>
+            <th align="center" colspan="<?= $colspan; ?>" style="border:0px;">
+                <h3>Progres Keuangan dan Fisik - <?= $title; ?></h3>
             </th>
         </tr>
         <tr>
-            <th align="center" colspan="<?=$colspan;?>" style="border:0px;">
-                <h3><?=$label;?></h3>
+            <th align="center" colspan="<?= $colspan; ?>" style="border:0px;">
+                <h3><?= $label; ?></h3>
             </th>
         </tr>
-        <tr><td colspan="<?=$colspan;?>">&nbsp;</td></tr>
+        <tr>
+            <td colspan="<?= $colspan; ?>">&nbsp;</td>
+        </tr>
     </table>
 
     <table class="table table-bordered">
         <tbody>
             <tr>
-                <td>Progres Keungan SDA</td><th><?=number_format( $qdata[0]['pagusda_progres_keuangan'],2,',','.');?>%</th>
+                <td>Progres Keungan SDA</td>
+                <th><?= number_format($qdata[0]['pagusda_progres_keuangan'], 2, ',', '.'); ?>%</th>
             </tr>
             <tr>
-                <td>Progres Fisik SDA</td><th><?=number_format( $qdata[0]['pagusda_progres_fisik'] ,2,',','.');?>%</th>
+                <td>Progres Fisik SDA</td>
+                <th><?= number_format($qdata[0]['pagusda_progres_fisik'], 2, ',', '.'); ?>%</th>
             </tr>
-            <tr><td colspan="2">&nbsp;</td></tr>
+            <tr>
+                <td colspan="2">&nbsp;</td>
+            </tr>
         </tbody>
     </table>
 
@@ -82,10 +160,10 @@
                 <th colspan="2">Deviasi</th>
             </tr>
             <tr class="text-center theader">
-                <th class=""><?=$title;?></th>
+                <th class=""><?= $title; ?></th>
                 <th class="tdNilai">Jml&nbsp;Paket
                     <!-- <br /><small title="Pagu SDA">Total SDA <i class="fa fa-angle-double-right"></i><i class="fa fa-angle-double-right"></i></small> -->
-                    </th>
+                </th>
 
                 <th class="tdNilai">RPM</th>
                 <th class="tdNilai">SBSN</th>
@@ -102,55 +180,56 @@
             </tr>
         </thead>
         <tbody id="tbody-utama">
-            <?php if($qdata): ?>
-            <?php
-            $total_pagu_rpm=0;
-            $total_pagu_sbsn=0;
-            $total_pagu_phln=0;
-            $total_pagu_total=0;
-            $total_real_total=0;
-            ?>
-            <?php
-            foreach($qdata as $key => $data): ?>
+            <?php if ($qdata) : ?>
+                <?php
+                $total_pagu_rpm = 0;
+                $total_pagu_sbsn = 0;
+                $total_pagu_phln = 0;
+                $total_pagu_total = 0;
+                $total_real_total = 0;
+                ?>
+                <?php
+                foreach ($qdata as $key => $data) : ?>
 
-            <!-- balai -->
-            <tr class="stw<?=$data['stw'];?>">
-                <td class="tdKodeLabel"><?php echo $data['label']; ?></td>
-                <td class="tdNilai text-center"><?php echo $data['jml_paket']; ?></td>
+                    <!-- balai -->
+                    <tr class="stw<?= $data['stw']; ?>">
+                        <td class="tdKodeLabel"><?php echo $data['label']; ?></td>
+                        <td class="tdNilai text-center"><?php echo $data['jml_paket']; ?></td>
 
-                <td class="tdNilai text-right"><?php echo $data['jml_pagu_rpm']; ?></td>
-                <td class="tdNilai text-right"><?php echo $data['jml_pagu_sbsn']; ?></td>
-                <td class="tdNilai text-right"><?php echo $data['jml_pagu_phln']; ?></td>
-                <td class="tdNilai text-right"><?php echo $data['jml_pagu_total']; ?></td>
+                        <td class="tdNilai text-right"><?php echo $data['jml_pagu_rpm']; ?></td>
+                        <td class="tdNilai text-right"><?php echo $data['jml_pagu_sbsn']; ?></td>
+                        <td class="tdNilai text-right"><?php echo $data['jml_pagu_phln']; ?></td>
+                        <td class="tdNilai text-right"><?php echo $data['jml_pagu_total']; ?></td>
 
-                <td class="tdNilai text-right"><?php echo $data['jml_real_total']; ?></td>
+                        <td class="tdNilai text-right"><?php echo $data['jml_real_total']; ?></td>
 
-                <td class="tdPersen text-right"><?php echo number_format($data['jml_progres_keuangan'] ,2,',','.'); ?></td>
-                <td class="tdPersen text-right"><?php echo number_format($data['jml_progres_fisik'],2,',','.'); ?></td>
+                        <td class="tdPersen text-right"><?php echo number_format($data['jml_progres_keuangan'], 2, ',', '.'); ?></td>
+                        <td class="tdPersen text-right"><?php echo number_format($data['jml_progres_fisik'], 2, ',', '.'); ?></td>
 
-                <td class="tdPersen text-right"><?php echo ( $data['jml_progres_fisik']>$data['jml_progres_keuangan'] ?  number_format($data['jml_persen_deviasi'],2,',','.') :'-' ); ?></td>
-                <td class="tdPersen text-right"><?php echo ( $data['jml_progres_fisik']>$data['jml_progres_keuangan'] ? $data['jml_nilai_deviasi'] :'-' ); ?></td>
-            </tr>
-            <?php
-            $total_pagu_rpm+= $data['jml_pagu_rpm'];
-            $total_pagu_sbsn+= $data['jml_pagu_sbsn'];
-            $total_pagu_phln+= $data['jml_pagu_phln'];
-            $total_pagu_total+= $data['jml_pagu_total'];
-            $total_real_total+= $data['jml_real_total'];
-            ?>
-            <?php endforeach; ?>
-            <tr style="background-color:#ccb3ff; border:2px solid #ccc;">
-                <td colspan="2" class="text-center">TOTAL</td>
-                <td class="tdNilai text-right"><?php echo $total_pagu_rpm; ?></td>
-                <td class="tdNilai text-right"><?php echo $total_pagu_sbsn; ?></td>
-                <td class="tdNilai text-right"><?php echo $total_pagu_phln; ?></td>
-                <td class="tdNilai text-right"><?php echo $total_pagu_total; ?></td>
-                <td class="tdNilai text-right"><?php echo $total_real_total; ?></td>
+                        <td class="tdPersen text-right"><?php echo ($data['jml_progres_fisik'] > $data['jml_progres_keuangan'] ?  number_format($data['jml_persen_deviasi'], 2, ',', '.') : '-'); ?></td>
+                        <td class="tdPersen text-right"><?php echo ($data['jml_progres_fisik'] > $data['jml_progres_keuangan'] ? round($data['jml_nilai_deviasi']) : '-'); ?></td>
+                    </tr>
+                    <?php
+                    $total_pagu_rpm += $data['jml_pagu_rpm'];
+                    $total_pagu_sbsn += $data['jml_pagu_sbsn'];
+                    $total_pagu_phln += $data['jml_pagu_phln'];
+                    $total_pagu_total += $data['jml_pagu_total'];
+                    $total_real_total += $data['jml_real_total'];
+                    ?>
+                <?php endforeach; ?>
+                <tr style="background-color:#ccb3ff; border:2px solid #ccc;">
+                    <td colspan="2" class="text-center">TOTAL</td>
+                    <td class="tdNilai text-right"><?php echo $total_pagu_rpm; ?></td>
+                    <td class="tdNilai text-right"><?php echo $total_pagu_sbsn; ?></td>
+                    <td class="tdNilai text-right"><?php echo $total_pagu_phln; ?></td>
+                    <td class="tdNilai text-right"><?php echo $total_pagu_total; ?></td>
+                    <td class="tdNilai text-right"><?php echo $total_real_total; ?></td>
 
-                <td colspan="4" class="tdPersen text-right">&nbsp;</td>
-            </tr>
+                    <td colspan="4" class="tdPersen text-right">&nbsp;</td>
+                </tr>
             <?php endif; ?>
         </tbody>
     </table>
 </body>
+
 </html>
--- app/Modules/Admin/Views/Paket/Rekap/Rekap-2.php
+++ app/Modules/Admin/Views/Paket/Rekap/Rekap-2.php
@@ -1,60 +1,124 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rekap-Monika</title>
     <style>
-    .table td, th{
-        padding:2px;
-        font-size:12px;
-        border:1px solid #333;
-    }
-    table a{color:#000099;}
-    table tr:hover{color:#000000; text-decoration: underline; font-weight: bold;}
+        .table td,
+        th {
+            padding: 2px;
+            font-size: 12px;
+            border: 1px solid #333;
+        }
 
-    .tdKodeLabel{width:500px;}
-    .tdLabelFull{}
-    .tdKode{width:200px;}
-    .tdLabel{width:300px;}
-    .tdTV{width:90px;}
-    .tdLokasi{width:120px;}
-    .tdJP{width:40px;}
-    .tdMP{width:40px;}
-    .tdNilai{width:100px;}
-    .tdPersen{width:50px;}
-    .tdPersen{width:50px;}
+        table a {
+            color: #000099;
+        }
 
-    .tdsbalai{background-color:#ffb366; color:#000;}
-    .tdsatker{background-color:#ffcc99;}
-    .tdgiat{background-color:#ddccff;}
-    .tdoutput{background-color:#ccb3ff;}
-    .tdsoutput{background-color:#bb99ff;}
-    .tdkomponen{background-color:#aa80ff;}
+        table tr:hover {
+            color: #000000;
+            text-decoration: underline;
+            font-weight: bold;
+        }
 
-    .theader th{background-color:#66ccff; color:#000;}
+        .tdKodeLabel {
+            width: 500px;
+        }
+
+        .tdLabelFull {}
+
+        .tdKode {
+            width: 200px;
+        }
+
+        .tdLabel {
+            width: 300px;
+        }
+
+        .tdTV {
+            width: 90px;
+        }
+
+        .tdLokasi {
+            width: 120px;
+        }
+
+        .tdJP {
+            width: 40px;
+        }
+
+        .tdMP {
+            width: 40px;
+        }
+
+        .tdNilai {
+            width: 100px;
+        }
+
+        .tdPersen {
+            width: 50px;
+        }
+
+        .tdPersen {
+            width: 50px;
+        }
+
+        .tdsbalai {
+            background-color: #ffb366;
+            color: #000;
+        }
+
+        .tdsatker {
+            background-color: #ffcc99;
+        }
+
+        .tdgiat {
+            background-color: #ddccff;
+        }
+
+        .tdoutput {
+            background-color: #ccb3ff;
+        }
+
+        .tdsoutput {
+            background-color: #bb99ff;
+        }
+
+        .tdkomponen {
+            background-color: #aa80ff;
+        }
+
+        .theader th {
+            background-color: #66ccff;
+            color: #000;
+        }
     </style>
 </head>
+
 <body>
-    <?php $colspan=11; ?>
+    <?php $colspan = 11; ?>
 
     <table class="table table-bordered">
         <tr>
-            <th align="center" colspan="<?=$colspan;?>" style="border:0px;">
-                <h3><?=$title;?> - Progres Keuangan dan Fisik</h3>
+            <th align="center" colspan="<?= $colspan; ?>" style="border:0px;">
+                <h3><?= $title; ?> - Progres Keuangan dan Fisik</h3>
             </th>
         </tr>
         <tr>
-            <th align="center" colspan="<?=$colspan;?>" style="border:0px;">
-                <h3><?=$label;?></h3>
+            <th align="center" colspan="<?= $colspan; ?>" style="border:0px;">
+                <h3><?= $label; ?></h3>
             </th>
         </tr>
         <tr>
-            <th align="center" colspan="<?=$colspan;?>" style="border:0px;">
-                <h3><?=$label2;?></h3>
+            <th align="center" colspan="<?= $colspan; ?>" style="border:0px;">
+                <h3><?= $label2; ?></h3>
             </th>
         </tr>
-        <tr><td colspan="<?=$colspan;?>">&nbsp;</td></tr>
+        <tr>
+            <td colspan="<?= $colspan; ?>">&nbsp;</td>
+        </tr>
     </table>
 
     <table class="table table-bordered table-striped" border="1">
@@ -90,69 +154,89 @@
             </tr>
         </thead>
         <tbody id="tbody-utama">
-            <?php if($qdata): ?>
-            <?php $balaiid=[]; $satkerid=[]; $idp=[]; $idg=[]; $ido=[]; $idso=[]; $idkk=[]; $colspan=11; ?>
-            <?php foreach($qdata as $key => $d): ?>
+            <?php if ($qdata) : ?>
+                <?php $balaiid = [];
+                $satkerid = [];
+                $idp = [];
+                $idg = [];
+                $ido = [];
+                $idso = [];
+                $idkk = [];
+                $colspan = 11; ?>
+                <?php foreach ($qdata as $key => $d) : ?>
 
-                <?php $balaiK=$d['balaiid'];?>
-                <?php if( !in_array( $balaiK, $balaiid ) ):?>
-                    <tr><td class="tdsbalai" colspan="<?php echo $colspan; ?>"><?php echo $d['balai'];?></td></tr>
-                    <?php $balaiid = array_merge( [ $balaiK ], $balaiid );?>
-                <?php endif; ?>
+                    <?php $balaiK = $d['balaiid']; ?>
+                    <?php if (!in_array($balaiK, $balaiid)) : ?>
+                        <tr>
+                            <td class="tdsbalai" colspan="<?php echo $colspan; ?>"><?php echo $d['balai']; ?></td>
+                        </tr>
+                        <?php $balaiid = array_merge([$balaiK], $balaiid); ?>
+                    <?php endif; ?>
 
-                <?php $satkerK=$d['satkerid'];?>
-                <?php if( !in_array( $satkerK, $satkerid ) ):?>
-                    <tr><td class="tdsatker" colspan="<?php echo $colspan; ?>"><?php echo $d['satkerid'] ." ". $d['satker'];?></td></tr>
-                    <?php $satkerid = array_merge( [ $satkerK ], $satkerid );?>
-                <?php endif; ?>
+                    <?php $satkerK = $d['satkerid']; ?>
+                    <?php if (!in_array($satkerK, $satkerid)) : ?>
+                        <tr>
+                            <td class="tdsatker" colspan="<?php echo $colspan; ?>"><?php echo $d['satkerid'] . " " . $d['satker']; ?></td>
+                        </tr>
+                        <?php $satkerid = array_merge([$satkerK], $satkerid); ?>
+                    <?php endif; ?>
 
-                <?php $idk=$d['programid']; ?>
+                    <?php $idk = $d['programid']; ?>
 
-                <?php $idk=$idk.'.'.$d['giatid'];?>
-                <?php if( !in_array( $idk, $idp ) ):?>
-                    <tr><td class="tdgiat" colspan="<?php echo $colspan; ?>"><?php echo $idk;?></td></tr>
-                    <?php $idp = array_merge( [ $idk ], $idp );?>
-                <?php endif; ?>
+                    <?php $idk = $idk . '.' . $d['giatid']; ?>
+                    <?php if (!in_array($idk, $idp)) : ?>
+                        <tr>
+                            <td class="tdgiat" colspan="<?php echo $colspan; ?>"><?php echo $idk; ?></td>
+                        </tr>
+                        <?php $idp = array_merge([$idk], $idp); ?>
+                    <?php endif; ?>
 
-                <?php $idk=$idk.'.'.$d['outputid'];?>
-                <?php if( !in_array( $idk, $ido ) ):?>
-                    <tr><td class="tdoutput" colspan="<?php echo $colspan; ?>"><?php echo $idk;?></td></tr>
-                    <?php $ido = array_merge( [ $idk ], $ido );?>
-                <?php endif; ?>
+                    <?php $idk = $idk . '.' . $d['outputid']; ?>
+                    <?php if (!in_array($idk, $ido)) : ?>
+                        <tr>
+                            <td class="tdoutput" colspan="<?php echo $colspan; ?>"><?php echo $idk; ?></td>
+                        </tr>
+                        <?php $ido = array_merge([$idk], $ido); ?>
+                    <?php endif; ?>
 
-                <?php $idk=$idk.'.'.$d['soutputid'];?>
-                <?php if( !in_array( $idk, $idso ) ):?>
-                    <tr><td class="tdsoutput" colspan="<?php echo $colspan; ?>"><?php echo $idk;?></td></tr>
-                    <?php $idso = array_merge( [ $idk ], $idso );?>
-                <?php endif; ?>
+                    <?php $idk = $idk . '.' . $d['soutputid']; ?>
+                    <?php if (!in_array($idk, $idso)) : ?>
+                        <tr>
+                            <td class="tdsoutput" colspan="<?php echo $colspan; ?>"><?php echo $idk; ?></td>
+                        </tr>
+                        <?php $idso = array_merge([$idk], $idso); ?>
+                    <?php endif; ?>
 
-                <?php $idk=$idk.'.'.$d['komponenid'];?>
-                <?php if( !in_array( $idk, $idkk ) ):?>
-                    <tr><td class="tdkomponen" colspan="<?php echo $colspan; ?>"><?php echo $idk;?></td></tr>
-                    <?php $idkk = array_merge( [ $idk ], $idkk );?>
-                <?php endif; ?>
+                    <?php $idk = $idk . '.' . $d['komponenid']; ?>
+                    <?php if (!in_array($idk, $idkk)) : ?>
+                        <tr>
+                            <td class="tdkomponen" colspan="<?php echo $colspan; ?>"><?php echo $idk; ?></td>
+                        </tr>
+                        <?php $idkk = array_merge([$idk], $idkk); ?>
+                    <?php endif; ?>
 
-                <tr>
-                <td class="tdKode"><?php echo $d['id'];?></td>
-                <td class="tdLabel"><?php echo $d['label'];?></td>
+                    <tr>
+                        <td class="tdKode"><?php echo $d['id']; ?></td>
+                        <td class="tdLabel"><?php echo $d['label']; ?></td>
 
-                <td class="tdNilai text-right"><?php echo $d['pagu_rpm'];?></td>
-                <td class="tdNilai text-right"><?php echo $d['pagu_sbsn'];?></td>
-                <td class="tdNilai text-right"><?php echo $d['pagu_phln'];?></td>
-                <td class="tdNilai text-right"><?php echo $d['pagu_total'];?></td>
+                        <td class="tdNilai text-right"><?php echo $d['pagu_rpm']; ?></td>
+                        <td class="tdNilai text-right"><?php echo $d['pagu_sbsn']; ?></td>
+                        <td class="tdNilai text-right"><?php echo $d['pagu_phln']; ?></td>
+                        <td class="tdNilai text-right"><?php echo $d['pagu_total']; ?></td>
 
-                <td class="tdNilai text-right"><?php echo $d['real_total'];?></td>
+                        <td class="tdNilai text-right"><?php echo $d['real_total']; ?></td>
 
-                <td class="tdPersen text-right"><?php echo number_format($d['progres_keuangan'] ,2,',','.');?></td>
-                <td class="tdPersen text-right"><?php echo number_format($d['progres_fisik'] ,2,',','.');?></td>
+                        <td class="tdPersen text-right"><?php echo number_format($d['progres_keuangan'], 2, ',', '.'); ?></td>
+                        <td class="tdPersen text-right"><?php echo number_format($d['progres_fisik'], 2, ',', '.'); ?></td>
 
-                <td class="tdPersen text-right"><?php echo ( $d['progres_fisik']>$d['progres_keuangan'] ? number_format($d['persen_deviasi'] ,2,',','.') :'-' );?></td>
-                <td class="tdNilai text-right"><?php echo ( $d['progres_fisik']>$d['progres_keuangan'] ? $d['nilai_deviasi'] :'-' );?></td>
-                </tr>
+                        <td class="tdPersen text-right"><?php echo ($d['progres_fisik'] > $d['progres_keuangan'] ? number_format($d['persen_deviasi'], 2, ',', '.') : '-'); ?></td>
+                        <td class="tdNilai text-right"><?php echo ($d['progres_fisik'] > $d['progres_keuangan'] ? round($d['nilai_deviasi']) : '-'); ?></td>
+                    </tr>
 
-            <?php endforeach; ?>
+                <?php endforeach; ?>
             <?php endif; ?>
         </tbody>
     </table>
 </body>
+
 </html>
