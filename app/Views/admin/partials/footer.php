c76f2ae760cd1588172f66c05b418337ed7bd60a
Septian Iqbal Pratama <septianiqbalpratama@gmail.com>
2022-01-14 17:05:34 +0700

    update footer

--- app/Views/admin/partials/footer.php
+++ app/Views/admin/partials/footer.php
@@ -1,7 +1,13 @@
 <div class="kt-container  kt-container--fluid ">
-	<div class="kt-footer__copyright">
+	<div class="kt-footer__copyright p-0">
 		<!-- <?= date('Y') ?> &nbsp;&copy;&nbsp;Direktorat Jenderal Sumber Daya Air -->
-
-		<img src="<?= base_url('images/footer.jpg'); ?>" class="w-100" alt="">
+		<div class="row">
+			<div class="col-md-6 p-0">
+				<img src="<?= base_url('images/footer-left.png'); ?>" class="img-footer" alt="">
+			</div>
+			<div class="col-md-6 p-0 text-right">
+				<img src="<?= base_url('images/footer-right.png'); ?>" class="img-footer" alt="">
+			</div>
+		</div>
 	</div>
 </div>
