f6268df348fcb61b0bfd55562b2eb33a7ef80b73
yusfi1997 <yusfiadil@gmail.com>
2022-10-25 10:57:22 +0700

    add tahun 2023

--- app/Modules/Admin/Views/Auth.php
+++ app/Modules/Admin/Views/Auth.php
@@ -55,7 +55,7 @@
 										<i class="text-white fas fa-calendar"></i>
 									</div>
 									<select class="form-control" name="tahun">
-										<?php for ($date = 2021; $date <= date("Y"); $date++) {
+										<?php for ($date = 2021; $date <= date("Y")+1; $date++) {
 										?>
 											<option value="<?= $date ?>" selected="<?= date('Y') ?>"><?= $date ?></option>
 										<?php } ?>
