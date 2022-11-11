a3416036b59ac5a87a24f04c1acd17c3e2b5d27a
HokiKaha <hokikaha@gmail.com>
2022-11-11 09:44:24 +0700

    BG-107

--- app/Modules/Admin/Views/DokumenPK/Rekap/Rekap-Eselon1.php
+++ app/Modules/Admin/Views/DokumenPK/Rekap/Rekap-Eselon1.php
@@ -7,15 +7,15 @@
     </head>
 
     <body>
-        <table class="table table-bordered">
+        <table class="table table-bordered" border=1>
             <thead>
                 <tr>
                     <th>NO</th>
                     <th>Tahun</th>
                     <th>Balai</th>
-                    <th>Satker</th>
                     <th>SP</th>
                     <th>Indikator SP</th>
+                    <th>Satker</th>
                     <th>SK</th>
                     <th>Indikator SK</th>
                     <th>Output</th>
@@ -24,6 +24,55 @@
                     <th>Satuan</th>
                 </tr>
             </thead>
+            <tbody>
+                <?php foreach($data as $key => $value) { ?>
+                    <tr>
+                        <td rowspan="<?php echo $value['rowspan'] ?>">
+                            <?php echo $key+1 ?>
+                        </td>
+                        <td rowspan="<?php echo $value['rowspan'] ?>">
+                            <?php echo $tahun ?>
+                        </td>
+                        <td rowspan="<?php echo $value['rowspan'] ?>">
+                            <?php echo $value['namaBalai'] ?>
+                        </td>
+                    <?php foreach($value['sp'] as $keySp => $valueSp) { ?>
+                        <?php if ($keySp >= 1) { ?> </tr><tr> <?php } ?>
+                            <td rowspan="<?php echo $valueSp['rowspan'] <= 0 ? 1 : $valueSp['rowspan'] ?>">
+                                <?php echo $valueSp['namaSp'] ?>
+                            </td>
+                        <?php foreach($valueSp['indikatorSp'] as $keyIndicatorSp => $valueIndicatorSp) { ?>
+                            <?php if ($keyIndicatorSp >= 1) { ?> </tr><tr> <?php } ?>
+                                <td rowspan="<?php echo $valueIndicatorSp['rowspan'] <= 0 ? 1 : $valueIndicatorSp['rowspan'] ?>">
+                                    <?php echo $valueIndicatorSp['title'] ?>
+                                </td>
+                            <?php foreach($valueIndicatorSp['satker'] as $keySatker => $valueSatker) { ?>
+                                <?php if ($keySatker >= 1) { ?> </tr><tr> <?php } ?>
+                                    <td rowspan="<?php echo $valueSatker['rowspan'] <= 0 ? 1 : $valueSatker['rowspan'] ?>">
+                                        <?php echo $valueSatker['namaSatker'] ?>
+                                    </td>
+                                <?php foreach($valueSatker['sk'] as $keySk => $valueSk) { ?>
+                                    <?php if ($keySk >= 1) { ?> </tr><tr> <?php } ?>
+                                        <td rowspan="<?php echo $valueSk['rowspan'] <= 0 ? 1 : $valueSk['rowspan'] ?>">
+                                            <?php echo $valueSk['namaSk'] ?>
+                                        </td>
+                                    <?php foreach($valueSk['indikatorSk'] as $keySkIndikator => $valueSkIndikator) { ?>
+                                        <?php if ($keySkIndikator >= 1) { ?> </tr><tr> <?php } ?>
+                                            <td>
+                                                <?php echo $valueSkIndikator['title'] ?>
+                                            </td>
+                                            <td><?php echo $valueSkIndikator['output'] ?></td>
+                                            <td><?php echo $valueSkIndikator['outputSatuan'] ?></td>
+                                            <td><?php echo $valueSkIndikator['outcome'] ?></td>
+                                            <td><?php echo $valueSkIndikator['outcomeSatuan'] ?></td>
+                                        </tr>
+                                    <?php } ?>
+                                <?php } ?>
+                            <?php } ?>
+                        <?php } ?>
+                    <?php } ?>
+                <?php } ?>
+            </tbody>
         </table>
     </body>
 </html>
