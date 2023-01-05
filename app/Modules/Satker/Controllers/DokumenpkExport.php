47e27c85bb4e8998bc12ea483e86523a12c11beb
yusfi1997 <yusfiadil@gmail.com>
2023-01-05 11:07:14 +0700

    dynamic angka dibelakang (cetak pdf)

--- app/Modules/Satker/Controllers/DokumenpkExport.php
+++ app/Modules/Satker/Controllers/DokumenpkExport.php
@@ -451,15 +451,21 @@ class DokumenpkExport extends \App\Controllers\BaseController
                 $targetValue = '';
                 switch ($_detailDokumenType) {
                     case 'target':
-                        $targetValue = rupiahFormat($data_targetValue['target_value'], false, 3) . ' ' . $data['target_satuan'];
+                        // $targetValue = rupiahFormat($data_targetValue['target_value'], false, 3) . ' ' . $data['target_satuan'];
+                        $targetValue = rtrim(rtrim(number_format($data_targetValue['target_value'], 3, ',', '.'), '0'), ','). ' ' . $data['target_satuan'];
+
                         break;
 
                     case 'outcome':
-                        $targetValue = rupiahFormat($data_targetValue['outcome_value'], false, 3) . ' ' . $data['outcome_satuan'];
+                        // $targetValue = rupiahFormat($data_targetValue['outcome_value'], false, 3) . ' ' . $data['outcome_satuan'];
+                        $targetValue = rtrim(rtrim(number_format($data_targetValue['outcome_value'], 3, ',', '.'), '0'), ',') . ' ' . $data['outcome_satuan'];
+
                         break;
 
                     case 'output':
-                        $targetValue = rupiahFormat($data_targetValue['target_value'], false, 3) . ' ' . $data['target_satuan'];
+                        // $targetValue = rupiahFormat($data_targetValue['target_value'], false, 3) . ' ' . $data['target_satuan'];
+                        $targetValue = rtrim(rtrim(number_format($data_targetValue['target_value'], 3, ',', '.'), '0'), ',') . ' ' . $data['target_satuan'];
+
 
                         break;
                     default:
