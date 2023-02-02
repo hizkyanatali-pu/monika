9684e6bfd275f175a2eadb51525db334214f6739
yusfi1997 <yusfiadil@gmail.com>
2023-02-02 10:22:59 +0700

    fixing layout export pdf

--- app/Modules/Satker/Controllers/DokumenpkExport.php
+++ app/Modules/Satker/Controllers/DokumenpkExport.php
@@ -169,7 +169,7 @@ class DokumenpkExport extends \App\Controllers\BaseController
 
     private function pdf_pageDokumenOpening($pdf, $dataDokumen)
     {
-        $pdf->SetMargins(70, 10, 70, 0);
+        $pdf->SetMargins(17, 10, 17, 0);
         $pdf->AddPage('L', 'A4');
 
         $pdf->SetFont('Arial', 'B', 50);
