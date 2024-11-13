351c5077a91c5b737ae4988bf59b6ecd41a18a09
Fahreza Maulana Wisnu Wardana <reza11.6a@gmail.com>
2024-11-13 08:28:32 +0700

    renstra js

--- app/Views/jspages/renstra_js.php
+++ app/Views/jspages/renstra_js.php
@@ -1591,7 +1591,7 @@
                                         data-outcome1satuan="${ data.outcome_satuan }"
                                         onkeyup="return this.value = formatRupiah(this.value, '')"
                                         ${data.template_id === '5' || data.template_id === '6'  || data.template_id === '9' ||data.template_id === '11' || data.template_id === '12' || data.template_id === '13'  || data.template_id === '14' || data.template_id === '15'  || data.template_id === '16'|| data.template_id === '17' || data.template_id === '18' || data.template_id === '19'|| data.template_id === '20' || data.template_id === '29' || _templateType === 'eselon2' ||  _tahun === '2023' ? '' :""}
-                                        readonly="true">
+                                        >
                                     <div class="input-group-append">
                                         <span class="input-group-text">${ data.outcome_satuan }</span>
                                     </div>
