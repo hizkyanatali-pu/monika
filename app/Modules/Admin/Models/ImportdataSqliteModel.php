426ca968cc4129132475be0ed15ea7818356ed2f
Nusantara Tama <teknis.nusantaratama@gmail.com>
2025-02-12 13:09:17 +0700

    fix pull sqlite

--- app/Modules/Admin/Models/ImportdataSqliteModel.php
+++ app/Modules/Admin/Models/ImportdataSqliteModel.php
@@ -9,13 +9,14 @@ class ImportdataSqliteModel extends Model
     protected $table      = 'monika_pull_sqlite';
     protected $primaryKey = 'idpull';
 
-    protected $allowedFields = ['idpull', 'nmfile', 'nmfileoriginal', 'sizefile', 'tbl_nm', 'tbl_size', 'tbl_row', 'tbl_dt', 'tbl_uid', 'st', 'import_dt', 'import_uid', 'aktif_dt', 'aktif_uid', 'in_dt', 'in_uid', 'aksi'];
+    protected $allowedFields = ['idpull', 'nmfile', 'nmfileoriginal', 'sizefile', 'tbl_nm', 'tbl_size', 'tbl_row', 'tbl_dt', 'tbl_uid', 'st', 'import_dt', 'import_uid', 'aktif_dt', 'aktif_uid', 'in_dt', 'in_uid', 'aksi', 'tahun'];
 
 
     public function getDok($w = "")
     {
         $query = $this->table($this->table)
             ->select($this->table . ".*")
+            ->where("tahun", session("userData.tahun"))
             ->where($this->table . '.aksi', 1);
         if ($w != "") $query->where($w);
         $query->orderBy($this->table . '.in_dt', 'DESC');
